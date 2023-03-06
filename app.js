4c628fa04ebb3ef492a66dd9d85e35792ebd10b8
jsweeneyguy <john.sweeney1@outlook.com>
2023-03-05 21:33:43 -0600

    changed port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import * as fs from "fs";
 
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 const tokenRendered = {};
 const imageRendered = {};
 
